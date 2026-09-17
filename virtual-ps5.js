6fdcc397d8dae19631ef2ea7d1b9779ea68999e5
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-17 21:09:53 +0100

    Load real player squad hub

--- virtual-ps5.js
+++ virtual-ps5.js
@@ -6,5 +6,6 @@ window.__obiGameLoader=true;
 function load(src){var s=document.createElement('script');s.src=src;s.async=false;document.head.appendChild(s)}
 load('virtual-ps5-base.js');
 load('real-players.js');
+load('real-squad-hub.js');
 load('multiplayer.js');
 })();
