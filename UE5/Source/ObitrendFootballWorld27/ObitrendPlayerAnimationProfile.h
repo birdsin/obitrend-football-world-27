f47e032cd0ec8b8d8b8bcddb5d1ece92815d8fc5
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-18 23:20:00 +0100

    Expand football animation profile for locomotion and match actions

--- UE5/Source/ObitrendFootballWorld27/ObitrendPlayerAnimationProfile.h
+++ UE5/Source/ObitrendFootballWorld27/ObitrendPlayerAnimationProfile.h
@@ -10,27 +10,51 @@ class OBITRENDFOOTBALLWORLD27_API UObitrendPlayerAnimationProfile : public UObje
     GENERATED_BODY()
 
 public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Locomotion")
+    TObjectPtr<class UBlendSpace> LocomotionBlendSpace;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Locomotion")
+    TObjectPtr<class UBlendSpace> StrafeBlendSpace;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Locomotion")
     TObjectPtr<class UAnimSequence> Idle;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Locomotion")
     TObjectPtr<class UAnimSequence> Walk;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Locomotion")
     TObjectPtr<class UAnimSequence> Run;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Locomotion")
     TObjectPtr<class UAnimSequence> Sprint;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Locomotion")
     TObjectPtr<class UAnimSequence> Turn;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Ball")
+    TObjectPtr<class UAnimSequence> Receive;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Ball")
+    TObjectPtr<class UAnimSequence> Dribble;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Ball")
     TObjectPtr<class UAnimSequence> Kick;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Ball")
     TObjectPtr<class UAnimSequence> Pass;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Ball")
     TObjectPtr<class UAnimSequence> Shoot;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Contact")
+    TObjectPtr<class UAnimSequence> Tackle;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Contact")
+    TObjectPtr<class UAnimSequence> Intercept;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Contact")
+    TObjectPtr<class UAnimSequence> ShoulderChallenge;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation|Goalkeeper")
+    TObjectPtr<class UAnimSequence> GoalkeeperSave;
 };
