a39a2d7d9bf3fc3eca66fa43b7babb248aa03f07
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-18 22:37:13 +0100

    Add realistic football game mode

---
+++ UE5/Source/ObitrendFootballWorld27/ObitrendFootballGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ObitrendFootballGameMode.generated.h"
+
+UCLASS()
+class OBITRENDFOOTBALLWORLD27_API AObitrendFootballGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AObitrendFootballGameMode();
+};
