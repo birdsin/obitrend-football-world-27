777b61c5b67e5e69dc97d2943a69dedff372b026
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-18 23:13:12 +0100

    Add directional turn data to locomotion runtime

--- UE5/Source/ObitrendFootballWorld27/ObitrendAnimationRuntimeComponent.cpp
+++ UE5/Source/ObitrendFootballWorld27/ObitrendAnimationRuntimeComponent.cpp
@@ -17,50 +17,40 @@ void UObitrendAnimationRuntimeComponent::ClearAction()
     bActionActive = false;
 
     if (MovementSpeed < 20.0f)
-    {
         AnimationState = EObitrendRuntimeAnimation::Idle;
-    }
     else if (MovementSpeed < 260.0f)
-    {
         AnimationState = EObitrendRuntimeAnimation::Walk;
-    }
     else if (MovementSpeed < 520.0f)
-    {
         AnimationState = EObitrendRuntimeAnimation::Run;
-    }
     else
-    {
         AnimationState = EObitrendRuntimeAnimation::Sprint;
-    }
 }
 
 void UObitrendAnimationRuntimeComponent::UpdateLocomotion(
     float Speed,
     float DirectionDegrees)
 {
+    const float PreviousDirection = MovementDirection;
+
     MovementSpeed = FMath::Max(0.0f, Speed);
     MovementDirection = FMath::UnwindDegrees(DirectionDegrees);
     SpeedNormalized = FMath::Clamp(MovementSpeed / 720.0f, 0.0f, 1.0f);
+    bMoving = MovementSpeed > 20.0f;
+
+    const float DeltaDirection =
+        FMath::FindDeltaAngleDegrees(PreviousDirection, MovementDirection);
+
+    TurnAmount = FMath::Clamp(DeltaDirection / 45.0f, -1.0f, 1.0f);
 
     if (bActionActive)
-    {
         return;
-    }
 
-    if (MovementSpeed < 20.0f)
-    {
+    if (!bMoving)
         AnimationState = EObitrendRuntimeAnimation::Idle;
-    }
     else if (MovementSpeed < 260.0f)
-    {
         AnimationState = EObitrendRuntimeAnimation::Walk;
-    }
     else if (MovementSpeed < 520.0f)
-    {
         AnimationState = EObitrendRuntimeAnimation::Run;
-    }
     else
-    {
         AnimationState = EObitrendRuntimeAnimation::Sprint;
-    }
 }
