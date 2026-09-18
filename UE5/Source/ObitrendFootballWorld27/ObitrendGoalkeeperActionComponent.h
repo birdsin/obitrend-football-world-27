ca20f1a2cbb2301c8be4d344c7520bbaaa538cc1
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-18 22:54:35 +0100

    Add goalkeeper save and dive action foundation

---
+++ UE5/Source/ObitrendFootballWorld27/ObitrendGoalkeeperActionComponent.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ObitrendGoalkeeperActionComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EObitrendGoalkeeperAction : uint8
+{
+    Ready,
+    Catch,
+    Parry,
+    DiveLeft,
+    DiveRight,
+    Punch
+};
+
+UCLASS(ClassGroup=(Football), Blueprintable, meta=(BlueprintSpawnableComponent))
+class OBITRENDFOOTBALLWORLD27_API UObitrendGoalkeeperActionComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UObitrendGoalkeeperActionComponent();
+
+    UFUNCTION(BlueprintCallable, Category="Football|Goalkeeper")
+    EObitrendGoalkeeperAction EvaluateSave(
+        AActor* BallActor,
+        const FVector& GoalCenter,
+        float GoalWidth = 732.0f);
+
+    UFUNCTION(BlueprintCallable, Category="Football|Goalkeeper")
+    bool ExecuteSave(
+        AActor* BallActor,
+        EObitrendGoalkeeperAction Action);
+
+    UFUNCTION(BlueprintPure, Category="Football|Goalkeeper")
+    EObitrendGoalkeeperAction GetLastAction() const { return LastAction; }
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Football|Goalkeeper")
+    float ReactionRadius = 1800.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Football|Goalkeeper")
+    float CatchSpeedLimit = 900.0f;
+
+private:
+    EObitrendGoalkeeperAction LastAction = EObitrendGoalkeeperAction::Ready;
+};
