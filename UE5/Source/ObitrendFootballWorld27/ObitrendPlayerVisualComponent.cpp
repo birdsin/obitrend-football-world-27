d9e6db663fe8a90e3dd21dd1f0fd03fd3f745b83
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-19 06:07:41 +0100

    Implement reusable realistic player visual application

--- UE5/Source/ObitrendFootballWorld27/ObitrendPlayerVisualComponent.cpp
+++ UE5/Source/ObitrendFootballWorld27/ObitrendPlayerVisualComponent.cpp
@@ -3,6 +3,30 @@
 #include "GameFramework/Character.h"
 #include "Components/SkeletalMeshComponent.h"
 
+void UObitrendPlayerVisualComponent::ApplyRealisticVisuals()
+{
+    ACharacter* Character = Cast<ACharacter>(GetOwner());
+    if (!Character) return;
+
+    ApplyRealisticVisuals();
+    return;
+
+    if (RealisticBodyMesh)
+    {
+        Mesh->SetSkeletalMesh(RealisticBodyMesh);
+    }
+
+    if (KitMaterial && Mesh->GetNumMaterials() > 0)
+    {
+        Mesh->SetMaterial(0, KitMaterial);
+    }
+
+    if (AnimationInstance)
+    {
+        Mesh->SetAnimInstanceClass(AnimationInstance->GetClass());
+    }
+}
+
 void UObitrendPlayerVisualComponent::BeginPlay()
 {
     Super::BeginPlay();
